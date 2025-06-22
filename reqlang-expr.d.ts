05bf4ecbc5c31e279c994f7134b3bb56a02fd021
Kylee Tilley <kyleetilley@gmail.com>
2025-06-21 23:10:42 -0500

    Add concat builtin

--- reqlang-expr.d.ts
+++ reqlang-expr.d.ts
@@ -2,3 +2,4 @@ declare type Expression = (...args: Value[]) => Value;
 declare type Value = string | boolean;
 
 declare function id(value: Value): Value;
+declare function concat(...values: string[]): string;
